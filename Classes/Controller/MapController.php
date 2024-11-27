b19a928877f6fc72e802033dfb44e9c5d1b8a330
Jonatan Jalle Steller <jonatan.steller@adwmainz.de>
2024-11-28 00:44:27 +0100

    Add cache clearing and annotate controllers

--- Classes/Controller/MapController.php
+++ Classes/Controller/MapController.php
@@ -11,6 +11,7 @@ namespace Digicademy\CHFMap\Controller;
 
 use Digicademy\CHFBase\Domain\Repository\AbstractResourceRepository;
 use Psr\Http\Message\ResponseInterface;
+use TYPO3\CMS\Core\Cache\CacheTag;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 defined('TYPO3') or die();
@@ -27,10 +28,23 @@ class MapController extends ActionController
         $this->abstractResourceRepository = $abstractResourceRepository;
     }
 
+    /**
+     * Show feature list
+     *
+     * @return ResponseInterface
+     */
     public function indexAction(): ResponseInterface
     {
+        // Get resource
         $resourceIdentifier = $this->settings['resource'];
         $this->view->assign('resource', $this->abstractResourceRepository->findByIdentifier($resourceIdentifier));
+
+        // Set cache tag
+        $this->request->getAttribute('frontend.cache.collector')->addCacheTags(
+            new CacheTag('chf')
+        );
+
+        // Create response
         return $this->htmlResponse();
     }
 }
